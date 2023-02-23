6084139944206f07e00bf22b15ef2cbab91edb18
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-02-23 06:10:41 -0400

    add app.js file for the hamburguer menu. Is working, but need more work. Also, change background color of body and menu

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+function menuOnClick() {
+    document.querySelector("#menu-bar").classList.toggle("change");
+    document.querySelector("#nav").classList.toggle("change");
+    document.querySelector("#menu-bg").classList.toggle("change-bg");
+  }
+  
+  
